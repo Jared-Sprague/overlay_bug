142ee621b66f1d7da274e35b96d6c90dedfd576a
Jared Sprague <jared@caramelcode.com>
2023-08-27 19:11:44 -0400

    Good reproducer

--- src/nested_build.sh
+++ src/nested_build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd /root/src || exit
 echo "Building nested image using podman with storage driver:"
 podman info | grep graphDriverName
 podman build -f Containerfile.copy -t website:latest .
+podman images
